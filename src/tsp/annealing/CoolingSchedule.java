99bf8505b16beacd7a2745df5d64c54ae1e26027
Jakub Wierzchowiec <kuba.wierzchowiec@interia.pl>
2022-01-05 20:05:18 +0100

    Finished geometric cooling and swap neighbourhoud. Implementerd linear cooling schedule

--- src/tsp/annealing/CoolingSchedule.java
+++ src/tsp/annealing/CoolingSchedule.java
@@ -1,7 +1,11 @@
 package tsp.annealing;
 
 public class CoolingSchedule {
-    static double geometric(double temperature, double alpha, int exponent) {
-        return temperature * Math.pow(alpha, exponent);
+    static double geometric(double temperature, int exponent) {
+        return temperature * Math.pow(0.99999, exponent);
+    }
+
+    static double linear(double temperature, int cycle) {
+        return temperature / (1 + 0.001 * cycle);
     }
 }
--- src/tsp/annealing/SimulatedAnnealing.java
+++ src/tsp/annealing/SimulatedAnnealing.java
@@ -8,21 +8,26 @@ import java.util.List;
 
 public class SimulatedAnnealing {
     public static int tsp(AdjacencyMatrix matrix) {
-        List<Integer> solution = NearestNeighbour.get(matrix);
-        double temperature = 2 * getCost(matrix, solution);
-
         int size = matrix.getSize();
-        for (int i = 0; temperature > 0.001; i++) {
-            for (int j = 0; j < size; j++) {
+        long timeLimit = getTimeLimit(size);
+
+        List<Integer> solution = NearestNeighbour.get(matrix);
+        double initialTemperature = 2 * getCost(matrix, solution);
+        double currentTemperature = initialTemperature;
+
+        long initialTime = System.currentTimeMillis();
+        for (int i = 0; System.currentTimeMillis() - initialTime < timeLimit; i++) {
+            for (int j = 0; j < size / 2; j++) {
                 List<Integer> permutation = Neighbourhood.swap(solution);
                 double difference = getCost(matrix, permutation) - getCost(matrix, solution);
                 if (difference < 0) {
+                    System.out.println(getCost(matrix, permutation));
                     solution = permutation;
-                } else if (Math.random() < Math.pow(Math.E, -difference / temperature)) {
+                } else if (Math.random() < Math.pow(Math.E, -difference / currentTemperature)) {
                     solution = permutation;
                 }
             }
-            temperature = CoolingSchedule.geometric(temperature, 0.999, i);
+            currentTemperature = CoolingSchedule.geometric(initialTemperature, i);
         }
         return getCost(matrix, solution);
     }
@@ -33,5 +38,9 @@ public class SimulatedAnnealing {
             cost += matrix.getData(list.get(i), list.get(i + 1));
         }
         return cost;
+    }
+
+    private static long getTimeLimit(int instanceSize) {
+        return instanceSize * 880L + 71689;
     }
 }
